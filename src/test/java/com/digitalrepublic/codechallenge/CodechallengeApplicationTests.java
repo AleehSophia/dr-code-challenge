40bd091e5645581481dc788a76230c829ca48335
AleehSophia <alessia.casado@gmail.com>
2022-10-07 18:49:17 -0300

    :tada: Initial commit

---
+++ src/test/java/com/digitalrepublic/codechallenge/CodechallengeApplicationTests.java
@@ -0,0 +1,13 @@
+package com.digitalrepublic.codechallenge;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class CodechallengeApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
