24eb62b1bf9cd9576205182fa261478af2d43396
AleehSophia <alessia.casado@gmail.com>
2022-10-07 19:40:53 -0300

    :sparkles:feat: import.sql file

---
+++ src/main/resources/import.sql
@@ -0,0 +1,4 @@
+INSERT INTO tb_name(name, cpf) VALUES ('Alessia Casado', '222.333.444-55');
+INSERT INTO tb_name(name, cpf) VALUES ('Maria Joaquina', '111.333.777-55');
+INSERT INTO tb_name(name, cpf) VALUES ('Joao Silva', '222.111.000-88');
+INSERT INTO tb_name(name, cpf) VALUES ('Lucas Santos', '790.555.777-99');
