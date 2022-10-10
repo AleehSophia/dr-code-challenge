fbb8b369bd3c70861a1d4e1ae80670ef46280506
AleehSophia <alessia.casado@gmail.com>
2022-10-10 18:06:45 -0300

    changing import.aql

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,6 +1,3 @@
-INSERT INTO tb_account(name, cpf) VALUES ('Alessia Casado', '222.333.444-55');
-INSERT INTO tb_account(name, cpf) VALUES ('Maria Joaquina', '111.333.777-55');
-INSERT INTO tb_account(name, cpf) VALUES ('Joao Silva', '222.111.000-88');
-INSERT INTO tb_account(name, cpf) VALUES ('Lucas Santos', '790.555.777-99');
+INSERT INTO tb_account(balance) values (300.00);
 
-INSERT INTO tb_transfer(account_id_from, account_id_to, amount, transfer_date) VALUES (2, 4, 300.00, TIMESTAMP WITH TIME ZONE '2020-07-13T20:50:07.12345Z');
+INSERT INTO tb_client(name, cpf) values ('Alessia Casado', '696.821.570-56');
