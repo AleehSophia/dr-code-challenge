a9fef3ed4a44202f2083d216e1fc749a24a48b8f
AleehSophia <alessia.casado@gmail.com>
2022-10-11 21:11:45 -0300

    chore: adding more data to base seed

--- src/main/resources/import.sql
+++ src/main/resources/import.sql
@@ -1,3 +1,17 @@
 INSERT INTO tb_account(balance) values (300.00);
+INSERT INTO tb_account(balance) values (600.00);
+INSERT INTO tb_account(balance) values (480.00);
+INSERT INTO tb_account(balance) values (500.00);
+INSERT INTO tb_account(balance) values (623.00);
+INSERT INTO tb_account(balance) values (754.00);
+INSERT INTO tb_account(balance) values (230.00);
+INSERT INTO tb_account(balance) values (460.00);
 
-INSERT INTO tb_client(name, cpf) values ('Alessia Casado', '696.821.570-56');
+INSERT INTO tb_client(name, cpf) values ('Blair Waldorf', '696.821.570-56');
+INSERT INTO tb_client(name, cpf) values ('Serena van der Woodsen', '866.118.820-22');
+INSERT INTO tb_client(name, cpf) values ('Dan Humphrey', '908.046.180-62');
+INSERT INTO tb_client(name, cpf) values ('Chuck Bass', '874.205.760-41');
+INSERT INTO tb_client(name, cpf) values ('Nate Archibald', '583.385.880-16');
+INSERT INTO tb_client(name, cpf) values ('Jenny Humphrey', '040.905.790-82');
+INSERT INTO tb_client(name, cpf) values ('Erick van der Woodsen', '542.552.010-72');
+INSERT INTO tb_client(name, cpf) values ('Vanessa Abrams', '277.822.290-14');
